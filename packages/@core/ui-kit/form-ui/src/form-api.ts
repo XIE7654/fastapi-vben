bf35c39933242a8b45b7cd91c88514c7ded725b7
xie7654 <765462425@qq.com>
2026-03-26 14:46:49 +0800

    修改：修改文件名

--- packages/@core/ui-kit/form-ui/src/form-api.ts
+++ packages/@core/ui-kit/form-ui/src/form-api.ts
@@ -289,7 +289,7 @@ export class FormApi {
 
   /**
    * 设置表单禁用状态：用于非 Modal 中使用 Form 时，需要 Form 自己控制禁用状态
-   * @author 芋道源码
+   * @author fastadmin源码
    * @param disabled 是否禁用
    */
   setDisabled(disabled: boolean) {
@@ -310,7 +310,7 @@ export class FormApi {
 
   /**
    * 设置表单提交按钮的加载状态：用于非 Modal 中使用 Form 时，需要 Form 自己控制 loading 状态
-   * @author 芋道源码
+   * @author fastadmin源码
    * @param loading 是否加载中
    */
   setLoading(loading: boolean) {
